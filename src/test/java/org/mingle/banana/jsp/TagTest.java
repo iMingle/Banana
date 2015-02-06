c49bd80072b2b909d89b556e809568a8f107e53e
jinminglei <jinminglei@yeah.net>
2015-02-06 21:26:35 +0800

    1. 文件夹占位文件建立

---
+++ src/test/java/org/mingle/banana/jsp/TagTest.java
@@ -0,0 +1,13 @@
+package org.mingle.banana.jsp;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TagTest extends TestCase {
+	
+	@Test
+	public void testExample() {
+		System.out.println("test");
+	}
+}
